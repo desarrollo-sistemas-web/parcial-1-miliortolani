e1dc2dfd789234b1d0160865725a61f1aeaf1c11
miliortolani <milagrosortolani729@gmail.com>
2026-05-28 20:47:09 -0300

    Add files via upload

--- ejercicio9.php
+++ ejercicio9.php
@@ -3,6 +3,9 @@
 //Ejercicio 9: Crea una función llamada generarTablaMultiplicar que tome un número entero como parámetro y mostrar los resultados (del 1 al 10) uno debajo del otro por pantalla.
 //Complejidad: BAJA (1 punto)
 
-function generarTablaMultiplicar($num) {
-    // Tu código aquí
+function generarTablaMultiplicar(int $num) {
+    for ($i=$1; $i<11;$i$num+) {
+    echo $i;    
+    echo "<br>";
+}
 }
